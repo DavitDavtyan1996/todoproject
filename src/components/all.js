9dcffdfbf96234dc315e4a803fea2ba315b9fcd0
Davit <davdyan.d0810@gmail.com>
2022-01-15 15:29:13 +0400

    Updates

--- src/components/all.js
+++
@@ -1,7 +0,0 @@
-import SearchPanel from "./search-panel/search-panel";
-import PostStatusFilter from "./post-status-filter/post-status-filter";
-import PostList from "./post-list/post-list";
-import PostAddForm from "./post-add-form/post-add-form";
-import AppHeader from "./app-header/app-header";
-
-export  {SearchPanel,PostStatusFilter,PostList,PostAddForm,AppHeader}
